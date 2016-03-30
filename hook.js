d89006c210e52a7d02b1fd6940a1806bdfc4d293
Konrad Bartecki <konradbartecki@outlook.com>
2016-03-30 21:48:14 +0200

    Create hook.js

---
+++ hook.js
@@ -0,0 +1,17 @@
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+gith({
+    repo: 'konradbartecki/Spotted.Service'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'master' )
+    {
+            // Exec a shell script
+            execFile('/home/antos/hook.sh', function(error, stdout, stderr) {
+                    // Log success in some manner
+                    console.log( 'exec complete' );
+            });
+    }
+});
