509e1d25a3f7939267bb0eb14b1299d546f7b83c
Konrad Bartecki <konradbartecki@outlook.com>
2016-03-30 21:55:40 +0200

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,5 +1,5 @@
 // Listen on port 9001
-var gith = require('gith').create( 9001 );
+var gith = require('gith').create(9000);
 // Import execFile, to run our bash script
 var execFile = require('child_process').execFile;
 
